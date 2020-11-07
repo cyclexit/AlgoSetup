b10ffe3116b0f2a002284d621ad8025c529c92a1
cyclexit <cyclexit@outlook.com>
2020-11-07 18:18:13 -0500

    Update linux set-up

---
+++ linux/contest/B/sol.cpp
@@ -0,0 +1,19 @@
+/*
+ * author: cyclexit
+ * start from: 2020-11-07 17:59
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  int a, b;
+  cin >> a >> b;
+  cout << a << " " << b << endl;
+  swap(a, b);
+  cout << a << " " << b << endl;
+  return 0;
+}
